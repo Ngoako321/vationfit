d2e97c9d2ea816da82d114e870e853e3774ee7f4
NgoakoM <4202406@myuwc.ac.za>
2026-03-09 01:18:19 +0200

    code

--- app.js
+++ app.js
@@ -4,10 +4,35 @@ function toggleMenu(hamburger) {
 }
 
 
-const buttons = document.querySelectorAll("addToCart");
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-buttons.forEach(function(button) {
-    button.addEventListener("click", function() {
-        alert("Item added to cart!");
-    });
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let username = document.getElementById("username").value;
+
+    let month = document.getElementById("month").value;
+    let day = document.getElementById("day").value;
+    let year = document.getElementById("year").value;
+
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+
+    if(name === ""  || surname === "" || username === ""){
+        alert("Please fill in all fields");
+        return;
+    }
+
+    if (month === "" || day === "" || year === ""){
+        alert("Please select your date of birth");
+        return;
+    }
+
+    if(password !== confirmPassword){
+        alert("Passwords do not match");
+        return;
+    }
+
+    alert("Account created successfully!")
+    window.location.href = "index.html";
 });
